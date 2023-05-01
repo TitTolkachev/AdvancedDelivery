37febc330d548c20ffafa1b25583e1aa57101fad
titto <tit.tolk07022003@gmail.com>
2023-05-01 20:57:32 +0700

    Начал делать Admin Panel

---
+++ AdminPanel.MVC/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanel.MVC.Controllers;
+
+public class ProfileController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ AdminPanel.MVC/Controllers/RestaurantController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanel.MVC.Controllers;
+
+public class RestaurantController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
