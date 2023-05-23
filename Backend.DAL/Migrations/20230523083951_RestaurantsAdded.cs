7095f06c233f4aa718a2b0fb9ca48b39815e2547
titto <tit.tolk07022003@gmail.com>
2023-05-23 15:49:28 +0700

    Миграции для ресторанов

---
+++ Backend.DAL/Migrations/20230523083951_RestaurantsAdded.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class RestaurantsAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CookId",
+                table: "Orders",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RestaurantId",
+                table: "Orders",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "MenuId",
+                table: "Dishes",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Restaurants",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cook",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    RestaurantId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cook", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cook_Restaurants_RestaurantId",
+                        column: x => x.RestaurantId,
+                        principalTable: "Restaurants",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Manager",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    RestaurantId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Manager", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Manager_Restaurants_RestaurantId",
+                        column: x => x.RestaurantId,
+                        principalTable: "Restaurants",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Menu",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    RestaurantId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Menu", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Menu_Restaurants_RestaurantId",
+                        column: x => x.RestaurantId,
+                        principalTable: "Restaurants",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CookId",
+                table: "Orders",
+                column: "CookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_RestaurantId",
+                table: "Orders",
+                column: "RestaurantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dishes_MenuId",
+                table: "Dishes",
+                column: "MenuId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cook_RestaurantId",
+                table: "Cook",
+                column: "RestaurantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Manager_RestaurantId",
+                table: "Manager",
+                column: "RestaurantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Menu_RestaurantId",
+                table: "Menu",
+                column: "RestaurantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Restaurants_Name",
+                table: "Restaurants",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Dishes_Menu_MenuId",
+                table: "Dishes",
+                column: "MenuId",
+                principalTable: "Menu",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Cook_CookId",
+                table: "Orders",
+                column: "CookId",
+                principalTable: "Cook",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Restaurants_RestaurantId",
+                table: "Orders",
+                column: "RestaurantId",
+                principalTable: "Restaurants",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Dishes_Menu_MenuId",
+                table: "Dishes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Cook_CookId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Restaurants_RestaurantId",
+                table: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Cook");
+
+            migrationBuilder.DropTable(
+                name: "Manager");
+
+            migrationBuilder.DropTable(
+                name: "Menu");
+
+            migrationBuilder.DropTable(
+                name: "Restaurants");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_CookId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_RestaurantId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Dishes_MenuId",
+                table: "Dishes");
+
+            migrationBuilder.DropColumn(
+                name: "CookId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "RestaurantId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "MenuId",
+                table: "Dishes");
+        }
+    }
+}
